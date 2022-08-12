7ea1a133caede4e6b97c8a9efb7662d84aaa1e9d
Nicolas <nicolas.parucker@hotmail.com>
2022-08-12 19:31:39 +0200

    cleaned up committed files

--- app/src/main/java/com/example/wordsearch/compose/HomeActivity.kt
+++ app/src/main/java/com/example/wordsearch/compose/HomeActivity.kt
@@ -1,19 +1,16 @@
 package com.example.wordsearch.compose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import com.example.wordsearch.data.DataMuseResult
-import com.example.wordsearch.model.WordItem
 
 const val EN_WIKI : String = "enwiki"
 const val ES_VOCAB : String = "es"
 
-class HomeActivity : AppCompatActivity(), DataMuseResult {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,15 +20,6 @@ class HomeActivity : AppCompatActivity(), DataMuseResult {
         }
     }
 
-    override fun onDataFetchedSuccess(words: List<WordItem>) {
-        Log.d("HomeActivity", "fetching  was successful" )
-//        TODO : implement logic when words are fetched successfully
-    }
-
-    override fun onDataFetchedFailed() {
-        Log.d("HomeActivity", "fetching failed" )
-        //        TODO : implement when logic the fetch query has failed
-    }
 }
 
 @Composable
