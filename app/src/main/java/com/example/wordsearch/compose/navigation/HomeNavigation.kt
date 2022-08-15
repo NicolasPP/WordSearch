41cd67e05dee927f611cd381f84550eb988a4589
Nicolas <nicolas.parucker@hotmail.com>
2022-08-15 19:20:25 +0200

    removed unused objects in Home navigation

---
+++ app/src/main/java/com/example/wordsearch/compose/navigation/HomeNavigation.kt
@@ -0,0 +1,14 @@
+package com.example.wordsearch.compose.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.wordsearch.R
+
+sealed class HomeNavigation(
+    val route: String,
+    @StringRes val stringResId : Int,
+    @DrawableRes val drawResId : Int
+) {
+    object LeaderBoard : HomeNavigation("LeaderBoard", R.string.LeaderBoard, R.drawable.trophy_icon_24)
+    object Home : HomeNavigation("Home", R.string.Settings, R.drawable.home_icon_24)
+}
